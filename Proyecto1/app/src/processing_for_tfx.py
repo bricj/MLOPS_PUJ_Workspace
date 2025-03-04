5fab776c6022b2fc26d084ece2b2fccd944c6cb0
Jose Campo <100389874+joseroca99@users.noreply.github.com>
2025-03-04 12:22:28 -0500

    comments py for Transform

--- Proyecto1/app/src/processing_for_tfx.py
+++ Proyecto1/app/src/processing_for_tfx.py
@@ -1,27 +1,24 @@
 import tensorflow as tf
 import tensorflow_transform as tft
 
+#variables numericas
 quantitative_variables = ['Elevation', 'Aspect', 'Slope', 'Horizontal_Distance_To_Hydrology',
        'Vertical_Distance_To_Hydrology', 'Horizontal_Distance_To_Roadways',
        'Hillshade_9am', 'Hillshade_Noon', 'Hillshade_3pm',
        'Horizontal_Distance_To_Fire_Points']
 
+#variables categoricas
 categorical_variables = ['Wilderness_Area', 'Soil_Type']
 
+#funcion para el artefacto Transform
 def preprocessing_fn(inputs):
-    """Preprocess input features using TF Transform.
-    
-    Inputs: Variables to convert
-    Outputs: Transformed variables
-    
-    """
     outputs = {}
 
-    # Normalize numerical features
+    # Normalizar variables numericas
     for col in quantitative_variables:
         outputs[col] = tft.scale_to_z_score(inputs[col])
 
-    # Convert categorical feature into one-hot encoding
+    # realizar one hot encoding a variables categoricas
     for col in categorical_variables:
         outputs[col] = tft.compute_and_apply_vocabulary(inputs[col])
 
