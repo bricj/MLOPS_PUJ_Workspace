a3d6ff1861a08770ed7edc1aa2f4da2387927c15
estudiante <joseroca99@hotmail.com>
2025-04-01 22:50:50 -0500

    registered models api

--- Proyecto2/api_read_models/app/main.py
+++ Proyecto2/api_read_models/app/main.py
@@ -1,5 +1,6 @@
-import os
 import joblib
+import mlflow.models
+import mlflow.tracking
 import pandas as pd
 import numpy as np
 from typing import List, Dict, Any
@@ -8,13 +9,14 @@ from pydantic import BaseModel
 import logging
 import os
 import mlflow
+import requests
 
 # Ruta donde están los modelos dentro del contenedor Docker
-MODEL_DIR = "http://10.43.101.166:5000"
+MODEL_DIR = "http://mlflow:5000"
 mlflow.set_tracking_uri(MODEL_DIR)
 
-import os
-os.environ['MLFLOW_S3_ENDPOINT_URL'] = "http://10.43.101.166:9000"
+# import os
+os.environ['MLFLOW_S3_ENDPOINT_URL'] = "http://minio:9000"
 os.environ['AWS_ACCESS_KEY_ID'] = 'admin'
 os.environ['AWS_SECRET_ACCESS_KEY'] = 'supersecret'
 
@@ -35,11 +37,12 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
-def load_model(model_name: str, version: int):
+def load_model(model_name: str):
     """Carga un modelo desde un archivo .pkl"""
 
+    
     model = mlflow.pyfunc.load_model(
-    model_uri=f"models:/{model_name}/{version}"
+    model_uri=f"models:/{model_name}/latest"
     )
     return model
 
@@ -47,20 +50,31 @@ def load_model(model_name: str, version: int):
 @app.get("/models")
 def list_models():
     """Lista los modelos disponibles en la carpeta /models"""
-    client = mlflow.tracking.MlflowClient()
-    # Listar modelos registrados en el Model Registry
-    models = client.search_registered_models()
-    model_info = [
-        {
-            "name": version.name,
-            "current_stage": version.current_stage,
-            "version": version.version
-        }
-        for model in models
-        for version in model.latest_versions  # Aquí accedemos a latest_versions correctamente
-    ]
+    client = mlflow.tracking.MlflowClient(tracking_uri="http://mlflow:5000") 
 
-    return {"available_models": model_info}
+    # Fetch all registered models
+    models = client.search_registered_models()
+
+    # Get the latest model version
+    latest_model_versions = client.search_model_versions(f"name='{models[0].name}'")
+    latest_version = max(int(m.version) for m in latest_model_versions)  # Get the highest version
+
+    print(latest_version)
+
+    # Load the MLmodel metadata
+    model_md = mlflow.models.Model.load(models[0].latest_versions[0].source)
+
+    # Get the model signature (schema)
+    signature = model_md.signature
+
+    if signature:
+        print("✅ Input Schema:", signature.inputs)
+        print("✅ Output Schema:", signature.outputs)
+    else:
+        print("❌ No schema found for this model.")    
+
+    # Print model names
+    return [model.name for model in models]
 
 @app.post("/predict/{model_name}")
 def predict(model_name: str, version: int ,input_data: PredictionInput):
