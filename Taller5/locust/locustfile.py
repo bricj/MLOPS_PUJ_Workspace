717ade576dcd43c254ed071cf31d1cd035c1381f
bricj <bricenojsebastian@javeriana.edu.co>
2025-04-12 01:52:27 -0500

    add log file and limitations (cpu-memory-replicates)

--- Taller5/locust/locustfile.py
+++ Taller5/locust/locustfile.py
@@ -1,5 +1,25 @@
 from locust import HttpUser, task, between
 from numpy import random as r
+import logging
+import time
+import os
+
+# Configurar logging
+logging.basicConfig(
+    filename="/app/locust_logs.log",
+    filemode="a",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+replicas = os.getenv("REPLICA_COUNT", "No especificado")
+cpu_limit = os.getenv("CPU_LIMIT", "No especificado")
+mem_limit = os.getenv("MEM_LIMIT", "No especificado")
+
+logging.info(f"Iniciando prueba - replicas: {replicas}")
+logging.info(f"Iniciando prueba - cpu: {cpu_limit}")
+logging.info(f"Iniciando prueba - memoria: {mem_limit}")
+
 
 class UsuarioDeCarga(HttpUser):
     wait_time = between(1, 2.5)
@@ -22,9 +42,17 @@ class UsuarioDeCarga(HttpUser):
         "Horizontal_Distance_To_Fire_Points": r.rand(),
         "Wilderness_Area": r.randint(1,high=3)
         }
-        # Enviar una petición POST al endpoint /predict
         
+        # Medir tiempo de respuesta
+        inicio = time.time()
+        response = self.client.post(f"/predict/{model_name}", json=payload)
+        fin = time.time()
+        duracion = round(fin - inicio, 4)  # en segundos
+
+        # Enviar una petición POST al endpoint /predict
         response = self.client.post(f"/predict/{model_name}", json=payload)
         # Opcional: validación de respuesta
-        if response.status_code != 200:
-            print("❌ Error en la inferencia:", response.text)
+        if response.status_code == 200:
+            logging.info(f"✅ Éxito - Tiempo: {duracion}s - Status: {response.status_code}")
+        else:
+            logging.error(f"❌ Error - Tiempo: {duracion}s - Status: {response.status_code} - Respuesta: {response.text} - Payload: {payload}")
