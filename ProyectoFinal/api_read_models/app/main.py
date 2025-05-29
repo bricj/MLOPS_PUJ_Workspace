960731a93b81ea90ec2383014993b75791ce7251
ogcorzo <ogcorzo@javeriana.edu.co>
2025-05-28 23:46:02 -0500

    update API in progress

--- ProyectoFinal/api_read_models/app/main.py
+++ ProyectoFinal/api_read_models/app/main.py
@@ -1,3 +1,8 @@
+import os
+os.environ['MLFLOW_S3_ENDPOINT_URL'] = "http://10.43.101.168:30901" #minio
+os.environ['AWS_ACCESS_KEY_ID'] = 'minioadmin'
+os.environ['AWS_SECRET_ACCESS_KEY'] = 'minioadmin123'
+
 import joblib
 import mlflow.models
 import mlflow.tracking
@@ -8,7 +13,6 @@ from typing import List, Dict, Any
 from fastapi import FastAPI, HTTPException, Request, Response
 from pydantic import BaseModel
 import logging
-import os
 import mlflow
 import requests
 from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
@@ -17,17 +21,13 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 
 # Ruta donde están los modelos dentro del contenedor Docker
-MODEL_DIR = "http://10.43.101.168:31485" #Mlflow
+MODEL_DIR = "http://10.43.101.168:30500" #Mlflow
 mlflow.set_tracking_uri(MODEL_DIR)
 client = mlflow.tracking.MlflowClient(tracking_uri=MODEL_DIR) 
 
-# import os
-os.environ['MLFLOW_S3_ENDPOINT_URL'] = "http://10.43.101.168:30855" #minio
-os.environ['AWS_ACCESS_KEY_ID'] = 'minioadmin'
-os.environ['AWS_SECRET_ACCESS_KEY'] = 'minioadmin123'
 
 # URL de la base de datos de logs
-DATABASE_URL = "postgresql://inference:inferencepass@10.43.101.166:5433/inference"
+DATABASE_URL = "postgresql://airflow:airflowpass@10.43.101.168:30543/airflow"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -38,22 +38,14 @@ predictions_table = Table(
     "predictions", metadata,
     Column("id", Integer, primary_key=True, index=True, autoincrement=True),
     Column("model_name", String),
-    Column("feature_1", Integer),
-    Column("feature_2", Integer),
-    Column("feature_3", Integer),
-    Column("feature_4", Integer),
-    Column("feature_5", Integer),
-    Column("feature_6", Integer),
-    Column("feature_7", Integer),
-    Column("feature_8", Integer),
-    Column("feature_9", Integer),
-    Column("feature_10", Integer),
-    Column("feature_11", Integer),
-    Column("feature_12", Integer),
-    Column("feature_13", Integer),
-    Column("feature_14", Integer),
-    Column("feature_15", Integer),
-    Column("label", String)
+    Column("brokered_by", Float),
+    Column("price", Float),
+    Column("bed", Float),
+    Column("bath", Float),
+    Column("acre_lot", Float),
+    Column("street", Float),
+    Column("zip_code", Float),
+    Column("house_size", Float)
 )
 
 # Crear la tabla si no existe
@@ -62,21 +54,14 @@ metadata.create_all(bind=engine)
 app = FastAPI()
 
 class PredictionInput(BaseModel):
-    feature_1: int
-    feature_2: int
-    feature_3: int
-    feature_4: int
-    feature_5: int
-    feature_6: int
-    feature_7: int
-    feature_8: int
-    feature_9: int
-    feature_10: int
-    feature_11: int
-    feature_12: int
-    feature_13: int
-    feature_14: int
-    feature_15: int
+    brokered_by: float
+    bed: float
+    bath: float
+    acre_lot: float
+    street: float
+    zip_code: float
+    house_size: float
+
 
 
 
@@ -91,12 +76,12 @@ def load_model(model_name: str):
     """Carga un modelo desde un archivo .pkl"""
 
     
-    model = mlflow.pyfunc.load_model(
-    model_uri=f"models:/{model_name}/latest"
+    model = mlflow.sklearn.load_model(
+    model_uri=f"models:/{model_name}/Production"
     )
     """
     model_uri=f"models:/{model_name}/latest"
-    model = mlflow.sklearn.load_model(model_uri)
+    model = mlflow.pyfunc.load_model(model_uri)
     """
     return model
 
@@ -116,19 +101,7 @@ def list_models():
             latest_version = max(int(m.version) for m in latest_model_versions)  # Get the highest version
             versions.append(latest_version)
             print(f'{model.name}:{latest_version}')
-
-        # Load the MLmodel metadata
-        model_md = mlflow.models.Model.load(models[0].latest_versions[0].source)
-
-        # Get the model signature (schema)
-        signature = model_md.signature
-
-        if signature:
-            print("✅ Input Schema:", signature.inputs)
-            print("✅ Output Schema:", signature.outputs)
-        else:
-            print("❌ No schema found for this model.")
-
+        
         out = [f'{model.name}:{vs}' for model, vs in zip(models,versions)]
     else:
         out = [] 
@@ -161,21 +134,14 @@ def predict(model_name: str,input_data: PredictionInput):
         # Convertir la entrada a DataFrame si el modelo requiere ese formato
         try:
             numerical = [
-                input_data.feature_1,
-                input_data.feature_2,
-                input_data.feature_3,
-                input_data.feature_4,
-                input_data.feature_5,
-                input_data.feature_6,
-                input_data.feature_7,
-                input_data.feature_8,
-                input_data.feature_9,
-                input_data.feature_10,
-                input_data.feature_11,
-                input_data.feature_12,
-                input_data.feature_13,
-                input_data.feature_14,
-                input_data.feature_15
+                input_data.brokered_by,
+                input_data.bed,
+                input_data.bath,
+                input_data.acre_lot,
+                input_data.street,
+                input_data.zip_code,
+                input_data.house_size
+
             ]
 
             numerical = np.array(numerical).reshape(1, -1)
@@ -185,22 +151,14 @@ def predict(model_name: str,input_data: PredictionInput):
             session = SessionLocal()
             ins = predictions_table.insert().values(
                 model_name=model_name,
-                feature_1=input_data.feature_1,
-                feature_2=input_data.feature_2,
-                feature_3=input_data.feature_3,
-                feature_4=input_data.feature_4,
-                feature_5=input_data.feature_5,
-                feature_6=input_data.feature_6,
-                feature_7=input_data.feature_7,
-                feature_8=input_data.feature_8,
-                feature_9=input_data.feature_9,
-                feature_10=input_data.feature_10,
-                feature_11=input_data.feature_11,
-                feature_12=input_data.feature_12,
-                feature_13=input_data.feature_13,
-                feature_14=input_data.feature_14,
-                feature_15=input_data.feature_15,
-                label=str(predictions)
+                brokered_by=input_data.brokered_by,
+                price=float(predictions),
+                bed=input_data.bed,
+                bath=input_data.bath,
+                acre_lot=input_data.acre_lot,
+                street=input_data.street,
+                zip_code=input_data.zip_code,
+                house_size=input_data.house_size
             )
             session.execute(ins)
             session.commit()
@@ -214,3 +172,35 @@ def predict(model_name: str,input_data: PredictionInput):
 @app.get("/metrics")
 def metrics():
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
+
+@app.get("/annotations")
+def annotations():
+
+    # Get experiment by name
+    experiment = client.get_experiment_by_name('argocd_experiment')
+    if experiment is None:
+        raise ValueError(f"Experiment argocd_experiment not found")
+        
+    # Search runs with the name filter
+    runs = client.search_runs(
+            experiment_ids=[experiment.experiment_id],
+            filter_string=f"tags.`mlflow.runName` = 'svm_validation'",
+            order_by=["attributes.start_time DESC"],
+            max_results=1
+        )
+        
+    if not runs:
+        print(f"No runs found with name containing '{run_name_pattern}'")
+
+    latest_run = runs[0]
+    run_id = latest_run.info.run_id
+        
+        # Get annotation if exists (adjust path as needed)
+    try:
+        annotation_content = mlflow.artifacts.load_text(
+                f"runs:/{run_id}/annotations/log.txt"
+            )
+        print(annotation_content)
+        return annotation_content
+    except Exception as e:
+        print(f"Annotation not found: {str(e)}")
