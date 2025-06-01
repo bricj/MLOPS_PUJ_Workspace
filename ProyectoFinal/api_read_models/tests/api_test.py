5cc9a1161c8ff2a4af75ebae9108887e8269638b
ogcorzo <ogcorzo@javeriana.edu.co>
2025-06-01 09:10:25 -0500

    create main_test.py for tests in github actions

--- ProyectoFinal/api_read_models/tests/api_test.py
+++ ProyectoFinal/api_read_models/tests/api_test.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import patch, MagicMock
 from fastapi.testclient import TestClient
-from app.main import app
+from app.main_test import app
 
 client = TestClient(app)
 
